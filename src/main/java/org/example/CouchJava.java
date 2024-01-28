035407fd8f4faaa0bcd5928ab049a07bafd2f96d
qescobar <quentin.escobar@etu.univ-cotedazur.fr>
2024-01-28 17:34:11 +0100

    test

--- src/main/java/org/example/CouchJava.java
+++ src/main/java/org/example/CouchJava.java
@@ -197,7 +197,247 @@ public class CouchJava {
         //teste avec id = 1
         Assurance _assu = assuranceDAO.getAssuById(1);
         System.out.println(_assu.getData());
-        */
+
+
+
+       //getAllVehicule
+        List<Vehicule> _vehicules = vehiculeDAO.getAllVehicule();
+        for (Vehicule vehicule : _vehicules) {
+            System.out.println(vehicule.getData());
+        }
+
+
+        //getVehiculeByModel
+        List<Vehicule> _vehicules = vehiculeDAO.getVehiculeByModel("M5");
+        for (Vehicule vehicule : _vehicules) {
+            System.out.println(vehicule.getData());
+        }
+
+        //getVehiculeByMarque
+        List<Vehicule> _vehicules = vehiculeDAO.getVehiculeByMarque("BMW");
+        for (Vehicule vehicule : _vehicules) {
+            System.out.println(vehicule.getData());
+        }
+
+        //getVehiculeByAgence
+        List<Vehicule> _vehicules = vehiculeDAO.getVehiculeByAgence(1);
+        for (Vehicule vehicule : _vehicules) {
+            System.out.println(vehicule.getData());
+        }
+
+        //getVehiculeByImat
+        Vehicule _vehicule = vehiculeDAO.getVehiculeByImat("JM1NC2EF4A0974457");
+        System.out.println(_vehicule.getData());
+
+        //getAllParking
+        List<Parking> _parkings = parkingDAO.getAllParking();
+        for (Parking parking : _parkings) {
+            System.out.println(parking.getData());
+        }
+
+        //getParkingByAgence
+        List<Parking> _parkings = parkingDAO.getParkingByAgence(1);
+        for (Parking parking : _parkings) {
+            System.out.println(parking.getData());
+        }
+
+        //getParkingByVehicule
+        List<Parking> _parkings = parkingDAO.getParkingByVehicule(1);
+        for (Parking parking : _parkings) {
+            System.out.println(parking.getData());
+        }
+
+        //getParkingsWithAvailablePlaces
+        List<Parking> _parkings = parkingDAO.getParkingsWithAvailablePlaces();
+        for (Parking parking : _parkings) {
+            System.out.println(parking.getData());
+        }
+
+        //getParkingsWithAvailablePlacesByAgence
+        List<Parking> _parkings = parkingDAO.getParkingsWithAvailablePlacesByAgence(1);
+        for (Parking parking : _parkings) {
+            System.out.println(parking.getData());
+        }
+
+        //getAllLocations
+        List<Location> _locations = locationDAO.getAllLocations();
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getLocationsByClient
+        List<Location> _locations = locationDAO.getLocationsByClient(1);
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getLocationsByVehicule
+        List<Location> _locations = locationDAO.getLocationsByVehicule(1);
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getLocationsByAgence
+        List<Location> _locations = locationDAO.getLocationsByAgence(1);
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+       //getLocationsByEmploye
+        List<Location> _locations = locationDAO.getLocationsByEmploye(1);
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getLocationSortedByPrice
+        List<Location> _locations = locationDAO.getLocationSortedByPrice();
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getLocationSortedByDate
+        List<Location> _locations = locationDAO.getLocationSortedByDate();
+        for (Location location : _locations) {
+            System.out.println(location.getData());
+        }
+
+        //getAllFactures
+        List<Facture> _factures = factureDAO.getAllFactures();
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+        }
+
+        //getFactureByLocation
+        List<Facture> _factures = factureDAO.getFactureByLocation(1);
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+        }
+
+        //getFactureById
+        List<Facture> _factures = factureDAO.getFactureById(1);
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+        }
+
+        //getFacturesSortedByPrices
+        List<Facture> _factures = factureDAO.getFacturesSortedByPrices();
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+        }
+
+        //getFacturesByDate
+        List<Facture> _factures = factureDAO.getFacturesByDate("7/16/2023");
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+            }
+
+        //getFacturesByDateAndAgence
+        List<Facture> _factures = factureDAO.getFacturesByDateAndAgence("7/16/2023",1);
+        for (Facture facture : _factures) {
+            System.out.println(facture.getData());
+        }
+
+        //getAllEmployer
+        List<Employer> _employers = employerDAO.getAllEmployer();
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployerById
+        List<Employer> _employers = employerDAO.getEmployerById(1);
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployerByJob
+        List<Employer> _employers = employerDAO.getEmployerByJob("chef");
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployersSortedByJob
+        List<Employer> _employers = employerDAO.getEmployersSortedByJob();
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployerByAgence
+        List<Employer> _employers = employerDAO.getEmployerByAgence(1);
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployerByAgenceAndJob
+        List<Employer> _employers = employerDAO.getEmployerByAgenceAndJob(1,"chef");
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployersSortedByAgence
+        List<Employer> _employers = employerDAO.getEmployersSortedByAgence();
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployersSortedByNom
+        List<Employer> _employers = employerDAO.getEmployersSortedByNom();
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getEmployersSortedByPrenom
+        List<Employer> _employers = employerDAO.getEmployersSortedByPrenom();
+        for (Employer employer : _employers) {
+            System.out.println(employer.getData());
+        }
+
+        //getAllClient
+        List<Client> _clients = clientDAO.getAllClient();
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientById
+        List<Client> _clients = clientDAO.getClientById(1);
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientByNom
+        List<Client> _clients = clientDAO.getClientByNom("Jean");
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientSortedByEmail
+        List<Client> _clients = clientDAO.getClientSortedByEmail();
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientSortedByNom
+        List<Client> _clients = clientDAO.getClientSortedByNom();
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientSortedByPrenom
+        List<Client> _clients = clientDAO.getClientSortedByPrenom();
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+        //getClientSortedByAdresse
+        List<Client> _clients = clientDAO.getClientSortedByAdresse();
+        for (Client client : _clients) {
+            System.out.println(client.getData());
+        }
+
+         */
+
+
+
+
 
     }
 
